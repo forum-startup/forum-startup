71d624e5689456e834e60df3ed29ca3e1f158b48
ivelinyanev <123307555+ivelinyanev@users.noreply.github.com>
2025-11-11 17:51:00 +0200

    resolve exceptions conflict

--- src/main/java/org/example/forumstartup/exceptions/DuplicateEntityException.java
+++ src/main/java/org/example/forumstartup/exceptions/DuplicateEntityException.java
@@ -2,6 +2,10 @@ package org.example.forumstartup.exceptions;
 
 public class DuplicateEntityException extends RuntimeException {
     public DuplicateEntityException(String message) {
-        super(message);
+        super(message)''
+    }
+
+    public DuplicateEntityException(String entityName, String attribute, String value) {
+        super(String.format("%s with %s '%s' already exists.", entityName, attribute, value));
     }
 }
